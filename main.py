ef7c2bf5a1afde427bf960ebdcfce8a39f14e974
hungcm95 <chuhung1195@gmail.com>
2023-08-30 17:27:40 +0700

    cm3

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request, Response
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/', methods=['POST'])
+def incoming():
+    print("post req")
+    return Response(status=200)
